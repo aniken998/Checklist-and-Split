0e809517caa0c33d27e6da37caeac76277c6010e
Poucheong <pochong@ucsc.edu>
2019-11-07 00:39:08 -0800

    added in sp

--- program/app/src/main/java/com/example/checklistandsplit/Add_To_Do_List.java
+++ program/app/src/main/java/com/example/checklistandsplit/Add_To_Do_List.java
@@ -1,8 +1,12 @@
 package com.example.checklistandsplit;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,8 +19,26 @@ public class Add_To_Do_List extends AppCompatActivity {
     }
 
     public void save(View view) {
-        Intent i = new Intent(this,Checklist_List.class);
-        startActivity(i);
+        SharedPreferences sp = this.getSharedPreferences("title", Context.MODE_PRIVATE);
+        String title = sp.getString("titles", "");
+        EditText todo = findViewById(R.id.todo);
+        EditText date = findViewById(R.id.date);
+        EditText time = findViewById(R.id.time);
+        if(title.equals("")) {
+            title = title + todo.getText().toString() + "`" + date.getText().toString() + "`" + time.getText().toString();
+            Log.d("D", title);
+            sp.edit().clear().commit();
+            sp.edit().putString("titles",title).apply();
+            this.finish();
+        } else {
+            title = title + "_" + todo.getText().toString() + "`" + date.getText().toString() + "`" + time.getText().toString();
+            Log.d("D",title);
+            sp.edit().clear().commit();
+            sp.edit().putString("titles",title).apply();
+            this.finish();
+        }
+        //Intent i = new Intent(this,Checklist_List.class);
+        //startActivity(i);
 
     }
 }
