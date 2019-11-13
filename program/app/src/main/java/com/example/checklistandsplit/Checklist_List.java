bdbdab8ecbc5959fe9424dc9c74a2e7291683a54
aniken998 <enzhou@ucsc.edu>
2019-11-12 20:19:11 -0800

    add combine checklist view and add to do to one activity.

--- program/app/src/main/java/com/example/checklistandsplit/Checklist_List.java
+++ program/app/src/main/java/com/example/checklistandsplit/Checklist_List.java
@@ -1,10 +1,19 @@
 package com.example.checklistandsplit;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class Checklist_List extends AppCompatActivity {
     @Override
@@ -14,7 +23,17 @@ public class Checklist_List extends AppCompatActivity {
     }
 
     public void float_button(View view) {
-        Intent i = new Intent(this, Add_To_Do_List.class);
-        startActivity(i);
+        CardView addJob = (CardView) findViewById(R.id.add_job);
+        addJob.setVisibility(View.VISIBLE);
+        FloatingActionButton add = (FloatingActionButton) findViewById(R.id.floatingActionButton);
+        add.hide();
+
+    }
+
+    public void save(View view) {
+        CardView addJob = (CardView) findViewById(R.id.add_job);
+        addJob.setVisibility(View.INVISIBLE);
+        FloatingActionButton add = (FloatingActionButton) findViewById(R.id.floatingActionButton);
+        add.show();
     }
 }
