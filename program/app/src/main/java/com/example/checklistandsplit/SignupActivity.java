c3cda67d33115bc5da34605a812d0f104cb3376d
aniken998 <enzhou@ucsc.edu>
2019-11-13 09:44:45 -0800

    implement user sign up and sign in

--- program/app/src/main/java/com/example/checklistandsplit/SignupActivity.java
+++ program/app/src/main/java/com/example/checklistandsplit/SignupActivity.java
@@ -1,17 +1,75 @@
 package com.example.checklistandsplit;
 
+import android.content.Intent;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class SignupActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    FirebaseDatabase database;
+    private DatabaseReference mReference;
+    private static final String TAG = "EmailPassword";
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sign_up);
+
+        mAuth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+        mReference = database.getReference("Users");
+
     }
 
+    public void sign_up(View view) {
+        EditText user_name = (EditText) findViewById(R.id.sign_up_user_name);
+        EditText user_password = (EditText) findViewById(R.id.sign_up_password);
 
+        mAuth.createUserWithEmailAndPassword(user_name.getText().toString(), user_password.getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            FirebaseDatabase.getInstance().getReference("Users")
+                                    .child(user.getUid())
+                                    .setValue(user.getEmail()).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    finish_sign_up();
+                                }
+                            });
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(SignupActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    private void finish_sign_up() {
+        Intent intent = new Intent(this, MainActivity.class);
+        finish();
+        startActivity(intent);
+    }
 }
